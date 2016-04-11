80f139c077c21bdd9caf4a821a84cc0886ad0caf
Ankit Verma <v.ankit001@gmail.com>
2016-04-11 10:57:25 -0500

    fixing search results and adding third details page

--- search_ui/views.py
+++ search_ui/views.py
@@ -57,7 +57,18 @@ def search(request):
 
 def display_tweet(request):
     print("request is coming to display tweet")
-    #json_data=json.loads(request.body)
-    # print(json_data)
-    # print(json_data['x'])
-    return render(request, 'search1/display.html')
+    if request.method == 'GET':
+        id = request.GET.get('q')
+        print(id)
+        solr = pysolr.Solr('http://localhost:8983/solr/gettingstarted/', timeout=10)
+        results = solr.search("id:"+id, **{'wt':'json'})
+        results=results.__dict__
+        tweet_data=results['docs'][0]
+        new_tweet={}
+        bad_chars="'[]"
+        for k, v in tweet_data.items():
+            newkey=str(k).replace('.', '_')
+            v=str(v)
+            for c in bad_chars: v = v.replace(c, "")
+            new_tweet[newkey]=v
+        return render(request, 'search1/display.haml', {'result':new_tweet})
